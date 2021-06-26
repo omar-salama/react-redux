ca6f8438173b059327e7c6da058d9bab9ae2a4d6
Omar Salama <omarzalama@gmail.com>
2021-06-26 11:28:16 +0200

    implemented search component view

--- ReactJS/node-react-registeration/client/src/components/Search.js
+++ ReactJS/node-react-registeration/client/src/components/Search.js
@@ -1,10 +1,18 @@
 const Search = () => {
-    return (
-      <div>
-        Search
+  return (
+    <div className="Search">
+      <div class="input-group mb-4 w-50 mx-auto">
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Find user by name"
+        />
+        <button class="btn btn-primary ps-4 pe-4">
+          <i class="bi bi-search"></i>
+        </button>
       </div>
-    );
-  }
-  
-  export default Search;
-  
+    </div>
+  );
+};
+
+export default Search;
