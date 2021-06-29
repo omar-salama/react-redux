16efbacbdeda19ec8c514a17d30aa7b5a19c9d4e
Omar Salama <31420520+omar-salama@users.noreply.github.com>
2021-06-30 00:13:27 +0200

    configured dotnev

--- ReactJS/node-react-registeration/server/db_connection.js
+++ ReactJS/node-react-registeration/server/db_connection.js
@@ -1,5 +1,7 @@
+const dotenv = require("dotenv")
+dotenv.config()
 const mongoose = require("mongoose");
 const url =
-  "mongodb+srv://osalama:OSalama_123@initialcluster.uen29.mongodb.net/RegisterMe?retryWrites=true&w=majority";
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@initialcluster.uen29.mongodb.net/RegisterMe?retryWrites=true&w=majority`;
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
