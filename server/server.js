c232ddde0e583114dc9717beca83b5ff51c3f2fd
Omar Salama <31420520+omar-salama@users.noreply.github.com>
2021-08-17 18:31:49 +0200

    serving client-side routes through NGINX instead of node

--- server/server.js
+++ server/server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const morgan = require("morgan");
-const path = require("path");
 const userRouter = require("./users/user.route");
 
 const app = express();
@@ -13,14 +12,5 @@ if (process.env.NODE_ENV === "development") {
 app.use(morgan("common"));
 app.use(express.json());
 app.use("/api", userRouter);
-
-// handling Client-Side Routes.
-if (process.env.NODE_ENV === "production") {
-  app.get("/*", function (req, res) {
-    res.sendFile(
-      path.resolve(__dirname, "..", "client", "build", "index.html")
-    );
-  });
-}
 
 module.exports = app;
