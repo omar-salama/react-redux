608c24f85f522128ee0eaf70fc90a11f075b8b21
Omar Salama <31420520+omar-salama@users.noreply.github.com>
2021-08-19 15:34:49 +0200

    fixed state management action on user delete request

--- client/src/reducers/users.js
+++ client/src/reducers/users.js
@@ -1,12 +1,14 @@
 export const users = (state = {}, actions) => {
   switch (actions.type) {
+    case "DELETE_USER":
+      const { list } = state;
+      return list.filter((element) => element !== actions.payload);
     case "USERS_LIST":
     case "NEW_USER":
-    case "DELETE_USER":
       return { ...state, list: actions.payload };
-      case "USER_DETAILS":
-      case "UPDATE_USER":
-      case "CLEAR_DETAILS":
+    case "USER_DETAILS":
+    case "UPDATE_USER":
+    case "CLEAR_DETAILS":
       return { ...state, details: actions.payload };
     default:
       return state;
