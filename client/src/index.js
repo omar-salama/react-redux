3923897babfb24595485cedf64cb6189bc3c1898
omarexi <omar@exinitic.com>
2023-06-04 23:51:59 +0300

    test git user

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <App /> 
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
