1a0992bb8a3f2ca892286b435231b8dff11e4e70
omar-salama <omarzalama@gmail.com>
2023-06-09 00:33:23 +0300

    moved to the new react 18 root API

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
+const container = document.getElementById("root")
+const root = createRoot(container)
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <Router>
       <App />
     </Router>
   </React.StrictMode>,
-  document.getElementById("root")
 );
