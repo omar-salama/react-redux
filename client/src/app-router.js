316b484445e36bee677cf4cd3d48b12afd366813
omar-salama <omarzalama@gmail.com>
2023-06-09 00:27:16 +0300

    used default export

--- client/src/app-router.js
+++ client/src/app-router.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import UserDetails from "./components/UserDetails";
-import { NotFound } from "./components/NotFound";
+import NotFound from "./components/NotFound";
 const AppRouter = () => {
   return (
       <Routes>
