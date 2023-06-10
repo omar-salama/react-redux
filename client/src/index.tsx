e3c0c18b9eeb4db47f9d143117d439fc88cd9852
omar-salama <omarzalama@gmail.com>
2023-06-10 20:34:51 +0300

    fixed root container error

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -6,7 +6,7 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
-const container = document.getElementById("root")
+const container = document.getElementById("root") as HTMLElement;
 const root = createRoot(container)
 
 root.render(
